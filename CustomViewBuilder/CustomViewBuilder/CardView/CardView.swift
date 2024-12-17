7f4ad25e20d50b478dace525a582f0883e5d7a15
Abhishek Gupta <abhisheknik23@gmail.com>
2024-12-17 17:17:53 +0530

    -Added custom card view.

--- CustomViewBuilder/CustomViewBuilder/CardView/CardView.swift
+++ CustomViewBuilder/CustomViewBuilder/CardView/CardView.swift
@@ -8,5 +8,25 @@
 import SwiftUI
 
 struct CardView<Content: View>: View {
+    let title: String
+    let content: Content
     
+    init(title: String, @ViewBuilder content: () -> Content) {
+        self.content = content()
+        self.title = title
+    }
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(title)
+                .font(.headline)
+                .padding(.bottom, 5)
+            VStack {
+                content
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(12)
+            .shadow(radius: 4)
+        }
+    }
 }
--- CustomViewBuilder/CustomViewBuilder/ContentView.swift
+++ CustomViewBuilder/CustomViewBuilder/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        CardView(title: "Custom Card View") {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
