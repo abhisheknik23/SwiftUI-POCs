77692d6d44c62fda0552a215dde0f18e8ae8a5aa
Abhishek Gupta <abhisheknik23@gmail.com>
2024-12-17 17:12:36 +0530

    -Implement and understatnd custom ViewBuilder.

---
+++ CustomViewBuilder/CustomViewBuilder/CardView/CardView.swift
@@ -0,0 +1,12 @@
+//
+//  CardView.swift
+//  CustomViewBuilder
+//
+//  Created by Abhishek Gupta on 11/12/24.
+//
+
+import SwiftUI
+
+struct CardView<Content: View>: View {
+    
+}
---
+++ CustomViewBuilder/CustomViewBuilder/CustomViewBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewBuilderApp.swift
+//  CustomViewBuilder
+//
+//  Created by Abhishek Gupta on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
